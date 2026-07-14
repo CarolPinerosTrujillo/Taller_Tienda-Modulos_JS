84120b8f5c522ca09e9cdd74c0aa8a630f5615b6
CarolPinerosTrujillo <caroll25m@gmail.com>
2026-07-14 18:30:08 -0500

    creacion carpeta aparte

---
+++ tienda/usuario.js
@@ -0,0 +1,17 @@
+//USUARIO
+
+class Usuario {
+    constructor(nombre, email, esVip) {
+        this.nombre = nombre; 
+        this.email =email ;
+        this.esVip =esVip ;
+
+    }
+    
+
+    saludo(){
+        return `Hola ${this.nombre}`
+    }
+}
+
+module.exports = Usuario; 
